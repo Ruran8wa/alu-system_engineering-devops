6f24154b5a4914f8170d95466d33326f41233db3
Prince Rurangwa <p.rurangwa@alustudent.com>
2024-03-25 15:32:56 +0200

    innit - fix indentation in =>

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-#fix our stack so that we get to 0
-
+# fix our stack so that we get to 0
 exec { 'fix_file':
-  onlyif => 'test -e /etc/default/nginx',
+  onlyif    => 'test -e /etc/default/nginx',
   command   => 'sed -i "5s/[0-9]\\+/$ (ulimit -n)/" /etc/default/nginx; service nginx restart',
   provider  => shell,
   logoutput => true,
